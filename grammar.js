73a86a35d187c25f51a0888a15e7359baafc949b
joker1007 <kakyoin.hierophant@gmail.com>
2023-11-15 04:17:00 +0900

    chore: delete unnecessary function

--- grammar.js
+++ grammar.js
@@ -324,18 +324,10 @@ module.exports = grammar({
   }
 });
 
-function sep(rule, separator) {
-  return optional(sep1(rule, separator));
-}
-
 function sep1(rule, separator) {
   return seq(rule, repeat(seq(separator, rule)));
 }
 
 function commaSep1(rule) {
   return sep1(rule, ',');
-}
-
-function commaSep(rule) {
-  return optional(commaSep1(rule));
 }
